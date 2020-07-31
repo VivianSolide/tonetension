eff440af2e0a8640d62d1f05c1c0925cb548cc72
Vivian Sarazin <vivian.sarazin.dev@gmail.com>
2020-07-31 11:39:39 +0200

    chore: :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  {{data.map((el) => el.rn)}}
+  {{data.map((el) => el.bn)}}
+  {{data.map((el) => el.gn)}}
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      await fetch(
+        "https://v2-api.sheety.co/612035a23ef10236acde20367e8a78e8/toneTension/5"
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          this.data = res[5];
+        })
+        .catch((err) => console.error(err));
+    },
+  },
+};
+</script>
